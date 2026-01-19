57c3ccb5d50aafcdc31fab437db3078cd7d529ed
yokobo103 <229598498+yokobo103@users.noreply.github.com>
2026-01-19 23:03:43 +0900

    message

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-import * as THREE from "https://unpkg.com/three@0.161.0/build/three.module.js";
-import { GLTFLoader } from "https://unpkg.com/three@0.161.0/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
 
 const video = document.getElementById("cam");
 const canvas = document.getElementById("gl");
